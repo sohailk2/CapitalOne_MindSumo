1c88eeeecb4f2c7938de1119c27fa440c2b49add
Sohail Khan <sohailk2@illinois.edu>
2019-11-02 13:06:12 -0500

    changed back to app

--- app/__init__.py
+++ app/__init__.py
@@ -6,25 +6,25 @@ from flask_migrate import Migrate
 
 from flask_login import LoginManager
 
-application = Flask(__name__)
-applicationlication.config.from_object(Config)
+app = Flask(__name__)
+app.config.from_object(Config)
 
-db = SQLAlchemy(application)
-migrate = Migrate(application, db)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-login = LoginManager(application)
+login = LoginManager(app)
 login.login_view = 'login'
 
 
-from application import routes, models
+from app import routes, models
 
-if not application.debug and not application.testing:
+if not app.debug and not app.testing:
         # ...
 
-        if application.config['LOG_TO_STDOUT']:
+        if app.config['LOG_TO_STDOUT']:
             # stream_handler = logging.StreamHandler()
             # stream_handler.setLevel(logging.INFO)
-            # application.logger.addHandler(stream_handler)
+            # app.logger.addHandler(stream_handler)
             pass
         else:
             # if not os.path.exists('logs'):
@@ -35,8 +35,8 @@ if not application.debug and not application.testing:
             #     '%(asctime)s %(levelname)s: %(message)s '
             #     '[in %(pathname)s:%(lineno)d]'))
             # file_handler.setLevel(logging.INFO)
-            # application.logger.addHandler(file_handler)
+            # app.logger.addHandler(file_handler)
             pass
 
-        # application.logger.setLevel(logging.INFO)
-        application.logger.info('Microblog startup')
+        # app.logger.setLevel(logging.INFO)
+        app.logger.info('Microblog startup')
